4ddcb76ef1089899f3803e1de13f4a0788460da7
Tim Logtenberg <lgg@st-maartenscollege.nl>
2020-06-26 11:39:31 +0000

    Raden of de leter in het woord zit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("hoi")
+woordomteraden = "helelangezin"
+letter = input("voer een letter in")
+
+if letter in woordomteraden:
+  print("letter zit er in!")
+else:
+  print("letter zit er niet in!")
+  
